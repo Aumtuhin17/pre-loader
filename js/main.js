7232ce898a927987b6338394ed5dbf5bfc8dab02
aumtuhin <dev.aumtuhin@gmail.com>
2016-10-30 22:51:43 +0600

    Awesome css preloader with javascript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+var loader = document.getElementById('loader');
+var youvideo = document.getElementById('youtube-video');
+window.addEventListener('load', function(){
+	loader.style.display = "none";
+		if (loader.style.display = "none") {
+	youvideo.style.display = "inline";
+}
+});
